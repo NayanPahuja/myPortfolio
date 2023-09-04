7ae56178432934f5234d6d97ddfc3d1e9c405279
Nayan Pahuja <nayanpahuja25@gmail.com>
2023-09-05 01:19:07 +0530

    'Optimizations'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Nayan | Portfolio',
-  description: 'Software Developer Portfolio',
+  description: 'Nayan Pahuja &apos;s  Software Developer Portfolio. This is where i showcase my projects and experience',
+  keywords: 'HTML,CSS,JAVASCRIPT,REACT,KOTLIN,APP DEVELOPER, WEB DEVELOPER, LEETCODE , DSA, STUDENT, NAYAN PAHUJA'
 }
 
 export default function RootLayout({
